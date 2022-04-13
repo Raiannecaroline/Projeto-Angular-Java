8150642c91275b90e5c1396ca0ffea1c86fa7ff2
Raiane Caroline Souza Teixeira Ferreira <raianecaroline.st@gmail.com>
2022-04-13 13:54:01 -0300

    create: Adicionando a interface do funcionário

--- Front-end-Angular/src/app/app-routing.module.ts
+++ Front-end-Angular/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'funcionario',
+    loadChildren: () => import('./funcionario/funcionario.module').then((m) => {return m.FuncionarioModule})
+  }
+]
 
 @NgModule({
   declarations: [],

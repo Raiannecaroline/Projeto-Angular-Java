49db8ba4a29eb16156c01fae04832e0305c599f5
Raiane Caroline Souza Teixeira Ferreira <raianecaroline.st@gmail.com>
2022-04-14 10:56:38 -0300

    create: Rota funcionário

--- Front-end-Angular/src/app/funcionario/funcionario-routing.module.ts
+++ Front-end-Angular/src/app/funcionario/funcionario-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NovoFuncionarioComponent } from './pages/novo-funcionario/novo-funcionario.component';
+import { ListarFuncionarioComponent } from './pages/listar-funcionario/listar-funcionario.component';
 
 const routes: Routes = [
   {
     path: 'novo-funcionario',
     component: NovoFuncionarioComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ListarFuncionarioComponent
   }
 ];
 

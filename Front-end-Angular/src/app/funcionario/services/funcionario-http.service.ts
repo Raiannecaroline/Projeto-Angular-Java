09dfa7c41cf5225d977af7280653882f8746ad27
Raiane Caroline Souza Teixeira Ferreira <raianecaroline.st@gmail.com>
2022-04-18 09:48:24 -0300

    create: método http

--- Front-end-Angular/src/app/funcionario/services/funcionario-http.service.ts
+++ Front-end-Angular/src/app/funcionario/services/funcionario-http.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FuncionarioHttpService {
 
+  private readonly baseUrl = 'http://localhost:8080/servicos/funcionario'
+
   constructor(
     private http: HttpClient
   ) { }
